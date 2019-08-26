ab93485b65bb85dbed1e6160803c0bdfbc9f9022
R <rob_abcba@yahoo.com>
2019-08-26 04:59:40 -0700

    InLineStyle flex justifyContent alignItems

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={ {padding: 30} }>
-      <View style={ {flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-      <TextInput placeholder="Course Goal" style={ {width: '80%', borderColor: "blue", borderWidth: 1} }/>
-      <Button title="Add" />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row"
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+          // flexDirection: 'row',
+          justifyContent: "center",
+          alignItems: "stretch"
+        }}
+      >
+        <TextInput
+          placeholder="Course 1 Goal"
+          style={{ 
+            // width: "80%", 
+            borderColor: "blue", 
+            borderWidth: 1 
+          }}
+        />
+        <Button title="Add1" />
+      </View>
+      <View
+        style={{
+          backgroundColor: "orange",
+          flex: 2,
+          // width: 100,
+          // height: 100,
+          // flexDirection: 'row',
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+                <TextInput
+          placeholder="Course 2 Goal"
+          style={{ width: "80%", borderColor: "blue", borderWidth: 1 }}
+        />
+        <Button title="Add2" />
+      </View>
+      <View
+        style={{
+          backgroundColor: "yellow",
+          flex: 1,
+          // flexDirection: 'row',
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+                <TextInput
+          placeholder="Course 3 Goal"
+          style={{ width: "80%", borderColor: "blue", borderWidth: 1 }}
+        />
+        <Button title="Add3" />
       </View>
     </View>
   );
