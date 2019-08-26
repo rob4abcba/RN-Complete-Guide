3c185108f075c93dab7c5f9098138d84bbdae000
R <rob_abcba@yahoo.com>
2019-08-26 11:31:52 -0700

    ScrollV to FlatL. item not string but object w key

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
 import { getOrientationAsync } from "expo/build/ScreenOrientation/ScreenOrientation";
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
+  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
+  const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
   };
 
   const addGoalHandler = () => {
     // console.log(enteredGoal);
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal] );
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: enteredGoal} ]);
   };
 
   return (
-    <View
-      style={styles.screen}
-    >
-      <View
-        style={styles.view1}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course 1 Goal"
           style={styles.text1}
@@ -30,23 +34,26 @@ export default function App() {
         />
         <Button title="Add1" onPress={addGoalHandler} />
       </View>
-      <ScrollView style>
-        {courseGoals.map( (goal) => (
-        <View key={goal} style={styles.listItem}>
-          <Text > {goal} </Text>
+      <FlatList 
+      keyExtractor={(item, index) => item.id}
+      data={courseGoals}
+      renderItem={itemData =>
+        (
+          <View style={styles.listItem}>
+            <Text> {itemData.item.value} </Text>
           </View>
-        ))}
-      </ScrollView>
-      </View>
+        )}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    padding: 50
     // flexDirection: "row"
   },
-  view1: {
+  inputContainer: {
     backgroundColor: "red",
     // width: 100,
     height: 100,
@@ -63,28 +70,28 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center"
   },
-  text1: { 
-    width: "80%", 
-    borderColor: "blue", 
+  text1: {
+    width: "80%",
+    borderColor: "blue",
     borderWidth: 1,
-    padding: 10 
+    padding: 10
   },
-  text2: { 
-    width: "80%", 
-    borderColor: "blue", 
-    borderWidth: 1 
+  text2: {
+    width: "80%",
+    borderColor: "blue",
+    borderWidth: 1
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   listItem: {
     padding: 10,
     margin: 5,
-    backgroundColor: '#ccc',
-    borderColor: 'green',
+    backgroundColor: "#ccc",
+    borderColor: "green",
     borderWidth: 2
   }
 });
