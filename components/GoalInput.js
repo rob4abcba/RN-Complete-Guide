872a69ee49674ed4e9ddc5418a01c810b7dc2c76
R <rob_abcba@yahoo.com>
2019-08-29 05:55:06 -0700

    Wrap GoalInput in Modal switch invisible & animate

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,40 +1,46 @@
 import React, { useState } from "react";
-import { View, StyleSheet, TextInput, Button } from "react-native";
+import { View, StyleSheet, TextInput, Button, Modal } from "react-native";
 
 const GoalInput = props => {
-    const [enteredGoal, setEnteredGoal] = useState("");
+  const [enteredGoal, setEnteredGoal] = useState("");
 
-    const goalInputHandler = enteredText => {
-        setEnteredGoal(enteredText);
-      };
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Type Item to Add to List"
-        style={styles.text1}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-      />
-      <Button title="Add an item" onPress={props.onAddGoalYo.bind(this, enteredGoal) } />
-    </View>
+    <Modal visible={props.visibleYo} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Type Item to Add to List"
+          style={styles.text1}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button
+          title="Add an item"
+          onPress={props.onAddGoalYo.bind(this, enteredGoal)}
+        />
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-    text1: {
-        // width: "80%",
-        borderColor: "blue",
-        borderWidth: 1,
-        padding: 10
-      },
+  text1: {
+    // width: "80%",
+    borderColor: "blue",
+    borderWidth: 1,
+    padding: 10
+  },
   inputContainer: {
     // backgroundColor: "red",
     // width: 100,
-    height: 100,
-    // flexDirection: 'row',
+    // height: 100,
+    flexDirection: 'column',
+    flex: 1,
     justifyContent: "center",
-    alignItems: "stretch"
+    alignItems: "center"
   }
 });
 
